646207d4ac49ce4107b078df18101120154500ca
roshini-lss <jeyaroshini@squareshift.co>
2024-06-13 15:16:00 +0530

    Maproute changes

--- src/components/Orders/WorkOrderDesc.jsx
+++ src/components/Orders/WorkOrderDesc.jsx
@@ -1,55 +1,67 @@
-import React from "react";
-import "./WorkOrderDesc.css";
+import React from "react"
+import "./WorkOrderDesc.css"
 
 const OrderDesc = ({ workOrders }) => {
-  const item = workOrders[0];
-  const viewMap = () => {
-    window.open('/view-map', '_blank');
-  }
+    const item = workOrders[0]
+    const stateToPass = {
+        latitude: item.breakdown_location_latitude,
+        longitude: item.breakdown_location_longitude,
+    }
 
-  return (
-    <>
-      {item ? (
-        <div className="desc-container">
-          <div className="desc-title">
-            {item.description_of_the_problem_code}
-          </div>
-          <hr></hr>
-          <div className="desc-card">
-            <div className="desc-col">
-              <h3 className="desc-sub">State</h3>
-              <h6>{item.State}</h6>
-            </div>
-            <div className="desc-col">
-              <h3 className="desc-sub">Longitude</h3>
-              <h6>{item.breakdown_location_latitude}</h6>
-            </div>
-            <div className="desc-col">
-              <h3 className="desc-sub">Latitude</h3>
-              <h6>{item.breakdown_location_longitude}</h6>
-            </div>
-            <div className="desc-col">
-              <h3 className="desc-sub">Trucks Assigned</h3>
-              <h6>{item.pta_truck}</h6>
-            </div>
-            <div className="desc-col">
-              <h3 className="desc-sub">Truck Predicted</h3>
-              <h6>{item.pta_truck_predicted}</h6>
-            </div>
-          </div>
-          <div className="view-button">
-          <button className="view-btn" onClick={viewMap}>View</button>
-          </div>
-          <hr></hr>
-          <div className="other-details">
-            <div>01/02/2024, there had a good condition</div>
-          </div>
-        </div>
-      ) : (
-        <div className="desc-none">None Found!</div>
-      )}
-    </>
-  );
-};
+    const viewMap = () => {
+        const queryString = new URLSearchParams(stateToPass).toString()
+        window.open(`/view-map?${queryString}`, "_blank")
+    }
 
-export default OrderDesc;
+    return (
+        <>
+            {item ? (
+                <div className="desc-container">
+                    <div className="desc-title">
+                        {item.description_of_the_problem_code}
+                    </div>
+                    <hr></hr>
+                    <div className="desc-card">
+                        <div className="desc-col">
+                            <h3 className="desc-sub">State</h3>
+                            <h6>{item.State}</h6>
+                        </div>
+                        <div className="desc-col">
+                            <h3 className="desc-sub">Longitude</h3>
+                            <h6>{item.breakdown_location_latitude}</h6>
+                        </div>
+                        <div className="desc-col">
+                            <h3 className="desc-sub">Latitude</h3>
+                            <h6>{item.breakdown_location_longitude}</h6>
+                        </div>
+                        <div className="desc-col">
+                            <h3 className="desc-sub">Trucks Assigned</h3>
+                            <h6>{item.pta_truck}</h6>
+                        </div>
+                        <div className="desc-col">
+                            <h3 className="desc-sub">Truck Predicted</h3>
+                            <h6>{item.pta_truck_predicted}</h6>
+                        </div>
+                    </div>
+                    <div className="view-button">
+                        <button className="view-btn" onClick={viewMap}>
+                            View
+                        </button>
+                    </div>
+                    <hr></hr>
+                    <div className="other-details">
+                        <div>01/02/2024, there had a good condition</div>
+                    </div>
+                </div>
+            ) : (
+                <div className="desc-none">None Found!</div>
+            )}
+        </>
+    )
+}
+
+export default OrderDesc
+
+
+
+
