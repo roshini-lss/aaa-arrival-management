a87cbd2f829caa26c294875f4ac69ed3e39c9ad1
roshini-lss <jeyaroshini@squareshift.co>
2024-06-13 17:44:18 +0530

    ui color change

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,97 +1,94 @@
-import React from "react"
-import "./Login.css"
-import { CgProfile } from "react-icons/cg"
-import { useForm } from "react-hook-form"
-import { useNavigate } from "react-router-dom"
-
+import React from "react";
+import "./Login.css";
+import { CgProfile } from "react-icons/cg";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { Toaster, toast } from "sonner";
 const Login = () => {
-    const techAdminUsername = "TechAdmin"
-    const techAdminPassword = "TechAdmin#1"
-    const callManagementUsername = "CallMgmt"
-    const callManagementPassword = "CallMgmt#1"
-
-    const navigate = useNavigate()
-
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm()
-
-    const onSubmit = (data) => {
-        if (
-            techAdminUsername === data.username &&
-            techAdminPassword === data.password
-        ) {
-            navigate("/by-region")
-        } else if (
-            callManagementUsername === data.username &&
-            callManagementPassword === data.password
-        ) {
-            navigate("/call-management-orders")
-        }
+  const techAdminUsername = "TechAdmin";
+  const techAdminPassword = "TechAdmin#1";
+  const callManagementUsername = "CallMgmt";
+  const callManagementPassword = "CallMgmt#1";
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    if (
+      techAdminUsername === data.username &&
+      techAdminPassword === data.password
+    ) {
+      navigate("/by-region");
+    } else if (
+      callManagementUsername === data.username &&
+      callManagementPassword === data.password
+    ) {
+      navigate("/call-management-orders");
+    } else {
+      toast.warning("Invalid Credentials");
     }
-
-    return (
-        <div className="container">
-            <div className="login-card-container">
-                <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
-                    <div className="login-icon">
-                        <CgProfile size={80} />
-                    </div>
-                    <div>
-                        <input
-                            className="login-input"
-                            type="text"
-                            name="username"
-                            placeholder="Username..."
-                            {...register("username", {
-                                required: "Username is required",
-                            })}
-                        />
-                        <p className="error-msg">{errors.username?.message}</p>
-                    </div>
-                    <div>
-                        <input
-                            className="login-input"
-                            type="password"
-                            name="password"
-                            placeholder="Password..."
-                            {...register("password", {
-                                required: "Password is required",
-                                minLength: {
-                                    value: 8,
-                                    message:
-                                        "Password must be at least 8 characters",
-                                },
-                                validate: {
-                                    uppercase: (value) =>
-                                        /[A-Z]/.test(value) ||
-                                        "Atleast one uppercase letter required",
-                                    lowercase: (value) =>
-                                        /[a-z]/.test(value) ||
-                                        "Password must contain at least one lowercase letter",
-                                    digit: (value) =>
-                                        /[0-9]/.test(value) ||
-                                        "Password must contain at least one digit",
-                                },
-                            })}
-                        />
-                        <p className="error-msg">
-                            {errors.password ? errors.password.message : ""}
-                        </p>
-                    </div>
-                    <button className="login-btn pointer" type="submit">
-                        Login
-                    </button>
-                    <div className="login-addons">
-                        <div className="pointer">Forgot Password?</div>
-                        <div className="pointer">Signup</div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    )
-}
-
-export default Login
+  };
+  return (
+    <div className="container">
+      <Toaster  position="top" />
+      <div className="login-card-container">
+        <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+          <div className="login-icon">
+            <CgProfile size={80} />
+          </div>
+          <div>
+            <input
+              className="login-input"
+              type="text"
+              name="username"
+              placeholder="Username..."
+              {...register("username", {
+                required: "Username is required",
+              })}
+            />
+            <p className="error-msg">{errors.username?.message}</p>
+          </div>
+          <div>
+            <input
+              className="login-input"
+              type="password"
+              name="password"
+              placeholder="Password..."
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 8,
+                  message: "Password must be at least 8 characters",
+                },
+                validate: {
+                  uppercase: (value) =>
+                    /[A-Z]/.test(value) ||
+                    "Atleast one uppercase letter required",
+                  lowercase: (value) =>
+                    /[a-z]/.test(value) ||
+                    "Password must contain at least one lowercase letter",
+                  digit: (value) =>
+                    /[0-9]/.test(value) ||
+                    "Password must contain at least one digit",
+                },
+              })}
+            />
+            <p className="error-msg">
+              {errors.password ? errors.password.message : ""}
+            </p>
+          </div>
+          <button className="login-btn pointer" type="submit">
+            Login
+          </button>
+          <div className="login-addons">
+            <div className="pointer">Forgot Password?</div>
+            <div className="pointer">Signup</div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default Login;
